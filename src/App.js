3d8a862df1cfd7fdadecc0ec6a7c9f7d669f315c
Matthew Davies <matthew_thompson_davies@hotmail.co.uk>
2017-10-03 12:13:04 +0100

    Adding some very basic UI elements from foundation framework

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // React Functionality
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import TransitionGroup from "react-transition-group/TransitionGroup";
 
 // Import abstracted components
 import AppHeader from './components/layout/AppHeader';
@@ -8,14 +9,58 @@ import AppFooter from './components/layout/AppFooter';
 import About from './components/About';
 import Login from './components/login/Login';
 
-// Import the CSS
-import './css/site.min.css';
-
 // Declarations for Routing
+const firstChild = props => {
+    const childrenArray = React.Children.toArray(props.children);
+    return childrenArray[0] || null;
+};
 const Home = () =>  (
     <div className="row">
         <div className="small-12 column">
-            Hello World
+            <div className="callout primary">
+                <h1>Welcome to my site</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore ducimus esse exercitationem natus,
+                    nobis sed totam vitae. Eveniet expedita possimus ratione saepe ullam? Libero, nisi.</p>
+            </div>
+            <div className="callout secondary">
+                <h2>This is a H2</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate dignissimos error ex fuga modi perspiciatis saepe tenetur, voluptas? Doloremque iste molestias nemo quasi repellat voluptate.</p>
+            </div>
+            <div className="row">
+                <div className="small-12 medium-4 large-4 column">
+                    <div className="card">
+                        <div className="card-divider">
+                            <h3>Im a card!</h3>
+                        </div>
+                        <img src="http://placehold.it/400x200" alt="Im a placeholder image"/>
+                        <div className="card-section">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia, quia!</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="small-12 medium-4 large-4 column">
+                    <div className="card">
+                        <div className="card-divider">
+                            <h3>Im a card!</h3>
+                        </div>
+                        <img src="http://placehold.it/400x200" alt="Im a placeholder image"/>
+                        <div className="card-section">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, mollitia!</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="small-12 medium-4 large-4 column">
+                    <div className="card">
+                        <div className="card-divider">
+                            <h3>Im a card!</h3>
+                        </div>
+                        <img src="http://placehold.it/400x200" alt="Im a placeholder image"/>
+                        <div className="card-section">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, dolorum?</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 );
@@ -39,6 +84,11 @@ export class App extends Component {
                     <div>
                         <AppHeader />
                         <div className="content">
+                            {/*<Route exact path="/" children={({ match, ...rest }) => (*/}
+                                    {/*<TransitionGroup component={firstChild}>*/}
+                                        {/*{match && <Home {...rest} />}*/}
+                                    {/*</TransitionGroup>*/}
+                                {/*)}/>*/}
                             <Route exact path="/" component={Home}/>
                             <Route path="/about" component={AboutMe}/>
                             <Route path="/login" component={LoginPage}/>
--- src/components/layout/AppFooter.js
+++ src/components/layout/AppFooter.js
@@ -5,9 +5,17 @@ export class AppFooter extends Component {
         return (
             <footer>
                 <div className="row">
-                    <div className="small-12 column">
+                    <div className="small-12 medium-6 large-6 column">
                         <h2>Credits</h2>
                         <p>Copyright 2017 Matthew Davies</p>
+                    </div>
+                    <div className="small-12 medium-6 large-6 column text-right">
+                        <p>
+                            <a href="https://github.com/twotoan/react-learning" target="_blank">
+                                <i className="fa fa-2x fa-github"></i>
+                                View me on gitHub!
+                            </a>
+                        </p>
                     </div>
                 </div>
             </footer>
