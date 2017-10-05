2ee9972cfadf841234c6b9da11ed0384e163ed1b
Matthew Davies <matthew_thompson_davies@hotmail.co.uk>
2017-10-05 10:39:50 +0100

    Mapping work - need to rename variables as appropriate but listing now works

--- src/components/player-database/DbHome.js
+++ src/components/player-database/DbHome.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-const players = [
+let news = [
     {id: 1, name: "Simple", age: "15", icon: "x", region: "EU"},
     {id: 2, name: "Flamie", age: "15", icon: "x", region: "EU"},
     {id: 3, name: "Seized", age: "15", icon: "x", region: "EU"},
@@ -11,46 +11,46 @@ const players = [
     {id: 8, name: "Skadoodle", age: "15", icon: "x", region: "NA"},
     {id: 9, name: "Autimatic", age: "15", icon: "x", region: "NA"}
 ];
-const playerlist = players.map((players) => <li>{players.id}, {players.region}, {players.icon}, {players.name}</li>);
 
-export class PlayerEntry extends Component {
+export class defaultView extends Component {
     render() {
-        return (
-            <li>
-                <div className="row">
-                    <div className="large-1 column">
-                        {players.id}
-                    </div>
-                    <div className="large-2 column">
-                        {players.region}
-                    </div>
-                    <div className="large-2 column">
-                        {players.icon}
-                    </div>
-                    <div className="large-4 column">
-                        {players.name}
-                    </div>
-                </div>
-            </li>
-        )
-    }
-}
-
-
-export class DbHome extends Component {
-    render() {
-        return (
-            <div>
-                <div className="row">
-                    <div className="small-11 column">
-                        <ul>
-                            { playerlist }
-                        </ul>
-                    </div>
+        return(
+            <div className="row">
+                <div className="small-12 column">
+                    <NewsList news={news} />
                 </div>
             </div>
         )
     }
 }
+export class NewsItem extends Component {
+    render() {
+        let data = this.props.data;
+        return (
+            <li>
+                <div className="row">
+                    <div className="large-1 column">{data.id}</div>
+                    <div className="large-2 column">{data.icon}</div>
+                    <div className="large-3 column">{data.name}</div>
+                    <div className="large-2 column">{data.region}</div>
+                    <div className="large-1 column">{data.age}</div>
+                </div>
+            </li>
+        );
+    }
+}
+export class NewsList extends Component {
+    render() {
+        let newsArr = this.props.news;    // This is our news array!
+        let listItems = newsArr.map((newsObj) => {
+            return <NewsItem key={newsObj.id} data={newsObj} />;    // I am passing in each news object as a prop called 'data'
+        });
+        return (
+            <ul className="playerList">
+                {listItems}
+            </ul>
+        );
+    }
+}
 
-export default DbHome;
+export default defaultView;
