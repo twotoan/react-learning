1c40127361fd78c3a4ab88cb3d5a917bf637d8ca
Matthew Davies <matthew_thompson_davies@hotmail.co.uk>
2017-10-06 09:28:52 +0100

    Travis configuration

--- src/components/player-database/DbHome.js
+++ src/components/player-database/DbHome.js
@@ -2,21 +2,26 @@ import React, { Component } from 'react';
 
 // The Players Array
 let players = [
-    { name: "Simple", age: "15", icon: "", region: "EU", team: "Natus Vincere"},
-    { name: "Flamie", age: "15", icon: "", region: "EU", team: "Natus Vincere"},
-    { name: "Seized", age: "15", icon: "", region: "EU", team: "Natus Vincere"},
-    { name: "Zeus", age: "15", icon: "", region: "EU", team: "Natus Vincere"},
-    { name: "Edward", age: "15", icon: "", region: "EU", team: "Natus Vincere"},
-    { name: "Tarik", age: "15", icon: "img/teams/c9.png", region: "NA", team: "Cloud9"},
-    { name: "Stewie2k", age: "15", icon: "img/teams/c9.png", region: "NA", team: "Cloud9"},
-    { name: "RUSH", age: "15", icon: "img/teams/c9.png", region: "NA", team: "Cloud9"},
-    { name: "Skadoodle", age: "15", icon: "img/teams/c9.png", region: "NA", team: "Cloud9"},
-    { name: "Autimatic", age: "15", icon: "img/teams/c9.png", region: "NA", team: "Cloud9"},
-    { name: "FNS", age: "15", icon: "", region: "NA", team: "CLG"},
-    { name: "nahtE", age: "15", icon: "", region: "NA", team: "CLG"},
-    { name: "ReltuC", age: "15", icon: "", region: "NA", team: "CLG"},
-    { name: "Koosta", age: "15", icon: "", region: "NA", team: "CLG"},
-    { name: "Rickeh", age: "15", icon: "", region: "NA", team: "CLG"}
+    { name: "Simple", age: "15", nationality: "UA", icon: "img/teams/.png", region: "EU", team: "Natus Vincere"},
+    { name: "Flamie", age: "15", nationality: "RU", icon: "img/teams/.png", region: "EU", team: "Natus Vincere"},
+    { name: "Seized", age: "15", nationality: "RU", icon: "img/teams/.png", region: "EU", team: "Natus Vincere"},
+    { name: "Zeus", age: "15", nationality: "RU", icon: "img/teams/.png", region: "EU", team: "Natus Vincere"},
+    { name: "Edward", age: "15", nationality: "UA", icon: "img/teams/.png", region: "EU", team: "Natus Vincere"},
+    { name: "Tarik", age: "15", nationality: "NA", icon: "img/teams/c9.png", region: "NA", team: "Cloud9"},
+    { name: "Stewie2k", age: "15", nationality: "NA", icon: "img/teams/c9.png", region: "NA", team: "Cloud9"},
+    { name: "RUSH", age: "15", nationality: "NA", icon: "img/teams/c9.png", region: "NA", team: "Cloud9"},
+    { name: "Skadoodle", age: "15", nationality: "NA", icon: "img/teams/c9.png", region: "NA", team: "Cloud9"},
+    { name: "Autimatic", age: "15", nationality: "NA", icon: "img/teams/c9.png", region: "NA", team: "Cloud9"},
+    { name: "FNS", age: "15", nationality: "CD", icon: "img/teams/.png", region: "NA", team: "CLG"},
+    { name: "nahtE", age: "15", nationality: "NA", icon: "img/teams/.png", region: "NA", team: "CLG"},
+    { name: "ReltuC", age: "15", nationality: "NA", icon: "img/teams/.png", region: "NA", team: "CLG"},
+    { name: "Koosta", age: "15", nationality: "NA", icon: "img/teams/.png", region: "NA", team: "CLG"},
+    { name: "Rickeh", age: "15", nationality: "AU", icon: "img/teams/.png", region: "NA", team: "CLG"},
+    { name: "Friberg", age: "15", nationality: "SE", icon: "img/teams/.png", region: "NA", team: "CLG"},
+    { name: "Magiskboy", age: "15", nationality: "DN", icon: "img/teams/.png", region: "NA", team: "CLG"},
+    { name: "HS", age: "15", nationality: "EA", icon: "img/teams/.png", region: "NA", team: "CLG"},
+    { name: "Allu", age: "15", nationality: "FI", icon: "img/teams/.png", region: "NA", team: "CLG"},
+    { name: "Mixwell", age: "15", nationality: "ES", icon: "img/teams/.png", region: "NA", team: "CLG"}
 ];
 
 // Render the element and pass it the players info to be used as props.
@@ -29,6 +34,7 @@ export class defaultView extends Component {
                         <div className="tableHeading">
                             <div className="large-2 column">Team</div>
                             <div className="large-3 column">Alias</div>
+                            <div className="large-2 column">Nationality</div>
                             <div className="large-2 column">Region</div>
                             <div className="large-1 column">Age</div>
                         </div>
@@ -54,6 +60,7 @@ export class PlayerEntry extends Component {
                         <img className="icon" src={data.icon} alt={data.team}/>
                     </div>
                     <div className="large-3 column">{data.name}</div>
+                    <div className="large-2 column">{data.nationality}</div>
                     <div className="large-2 column">{data.region}</div>
                     <div className="large-1 column">{data.age}</div>
                 </div>
