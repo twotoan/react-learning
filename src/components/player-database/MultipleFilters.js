026d02e595dc090e851f3d66abbc9a87481a2f92
Matthew Davies <matthew_thompson_davies@hotmail.co.uk>
2017-10-11 11:16:14 +0100

    UI stuff - team icons and region images added

--- src/components/player-database/MultipleFilters.js
+++ src/components/player-database/MultipleFilters.js
@@ -13,31 +13,31 @@ export class FilteredList extends Component {
         super(props);
         this.state = {
             playersList: [
-                { name: "Simple", age: "15", nationality: "UA", icon: "img/teams/.png", region: "EU", team: "Natus Vincere"},
-                { name: "Flamie", age: "15", nationality: "RU", icon: "img/teams/.png", region: "EU", team: "Natus Vincere"},
-                { name: "Seized", age: "15", nationality: "RU", icon: "img/teams/.png", region: "EU", team: "Natus Vincere"},
-                { name: "Zeus", age: "15", nationality: "RU", icon: "img/teams/.png", region: "EU", team: "Natus Vincere"},
-                { name: "Edward", age: "15", nationality: "UA", icon: "img/teams/.png", region: "EU", team: "Natus Vincere"},
-                { name: "Tarik", age: "15", nationality: "NA", icon: "img/teams/c9.png", region: "NA", team: "Cloud9"},
-                { name: "Stewie2k", age: "15", nationality: "NA", icon: "img/teams/c9.png", region: "NA", team: "Cloud9"},
-                { name: "RUSH", age: "15", nationality: "NA", icon: "img/teams/c9.png", region: "NA", team: "Cloud9"},
-                { name: "Skadoodle", age: "15", nationality: "NA", icon: "img/teams/c9.png", region: "NA", team: "Cloud9"},
-                { name: "Autimatic", age: "15", nationality: "NA", icon: "img/teams/c9.png", region: "NA", team: "Cloud9"},
-                { name: "FNS", age: "15", nationality: "CD", icon: "img/teams/.png", region: "NA", team: "CLG"},
-                { name: "nahtE", age: "15", nationality: "NA", icon: "img/teams/.png", region: "NA", team: "CLG"},
-                { name: "ReltuC", age: "15", nationality: "NA", icon: "img/teams/.png", region: "NA", team: "CLG"},
-                { name: "Koosta", age: "15", nationality: "NA", icon: "img/teams/.png", region: "NA", team: "CLG"},
-                { name: "Rickeh", age: "15", nationality: "AU", icon: "img/teams/.png", region: "NA", team: "CLG"},
-                { name: "Friberg", age: "15", nationality: "SE", icon: "img/teams/.png", region: "NA", team: "Optic"},
-                { name: "Magiskboy", age: "15", nationality: "DN", icon: "img/teams/.png", region: "NA", team: "Optic"},
-                { name: "HS", age: "15", nationality: "EA", icon: "img/teams/.png", region: "NA", team: "Optic"},
-                { name: "Allu", age: "15", nationality: "FI", icon: "img/teams/.png", region: "NA", team: "Optic"},
-                { name: "Mixwell", age: "15", nationality: "ES", icon: "img/teams/.png", region: "NA", team: "Optic"},
-                { name: "Device", age: "15", nationality: "DN", icon: "img/teams/.png", region: "EU", team: "Astralis"},
-                { name: "Dupreeh", age: "15", nationality: "DN", icon: "img/teams/.png", region: "EU", team: "Astralis"},
-                { name: "Xypex", age: "15", nationality: "DN", icon: "img/teams/.png", region: "EU", team: "Astralis"},
-                { name: "Gla1ve", age: "15", nationality: "DN", icon: "img/teams/.png", region: "EU", team: "Astralis"},
-                { name: "Kjaerbye", age: "15", nationality: "DN", icon: "img/teams/.png", region: "EU", team: "Astralis"}
+                { name: "Simple", age: "15", nationality: "UA", icon: "img/teams/navi.png", region: "img/eu.gif", team: "Natus Vincere"},
+                { name: "Flamie", age: "15", nationality: "RU", icon: "img/teams/navi.png", region: "img/eu.gif", team: "Natus Vincere"},
+                { name: "Seized", age: "15", nationality: "RU", icon: "img/teams/navi.png", region: "img/eu.gif", team: "Natus Vincere"},
+                { name: "Zeus", age: "15", nationality: "RU", icon: "img/teams/navi.png", region: "img/eu.gif", team: "Natus Vincere"},
+                { name: "Edward", age: "15", nationality: "UA", icon: "img/teams/navi.png", region: "img/eu.gif", team: "Natus Vincere"},
+                { name: "Tarik", age: "15", nationality: "NA", icon: "img/teams/c9.png", region: "img/na.gif", team: "Cloud9"},
+                { name: "Stewie2k", age: "15", nationality: "NA", icon: "img/teams/c9.png", region: "img/na.gif", team: "Cloud9"},
+                { name: "RUSH", age: "15", nationality: "NA", icon: "img/teams/c9.png", region: "img/na.gif", team: "Cloud9"},
+                { name: "Skadoodle", age: "15", nationality: "NA", icon: "img/teams/c9.png", region: "img/na.gif", team: "Cloud9"},
+                { name: "Autimatic", age: "15", nationality: "NA", icon: "img/teams/c9.png", region: "img/na.gif", team: "Cloud9"},
+                { name: "FNS", age: "15", nationality: "CD", icon: "img/teams/clg.png", region: "img/na.gif", team: "CLG"},
+                { name: "nahtE", age: "15", nationality: "NA", icon: "img/teams/clg.png", region: "img/na.gif", team: "CLG"},
+                { name: "ReltuC", age: "15", nationality: "NA", icon: "img/teams/clg.png", region: "img/na.gif", team: "CLG"},
+                { name: "Koosta", age: "15", nationality: "NA", icon: "img/teams/clg.png", region: "img/na.gif", team: "CLG"},
+                { name: "Rickeh", age: "15", nationality: "AU", icon: "img/teams/clg.png", region: "img/na.gif", team: "CLG"},
+                { name: "Friberg", age: "15", nationality: "SE", icon: "img/teams/optic.png", region: "img/na.gif", team: "Optic"},
+                { name: "Magiskboy", age: "15", nationality: "DN", icon: "img/teams/optic.png", region: "img/na.gif", team: "Optic"},
+                { name: "HS", age: "15", nationality: "EA", icon: "img/teams/optic.png", region: "img/na.gif", team: "Optic"},
+                { name: "Allu", age: "15", nationality: "FI", icon: "img/teams/optic.png", region: "img/na.gif", team: "Optic"},
+                { name: "Mixwell", age: "15", nationality: "ES", icon: "img/teams/optic.png", region: "img/na.gif", team: "Optic"},
+                { name: "Device", age: "15", nationality: "DN", icon: "img/teams/astralis.png", region: "img/eu.gif", team: "Astralis"},
+                { name: "Dupreeh", age: "15", nationality: "DN", icon: "img/teams/astralis.png", region: "img/eu.gif", team: "Astralis"},
+                { name: "Xypex", age: "15", nationality: "DN", icon: "img/teams/astralis.png", region: "img/eu.gif", team: "Astralis"},
+                { name: "Gla1ve", age: "15", nationality: "DN", icon: "img/teams/astralis.png", region: "img/eu.gif", team: "Astralis"},
+                { name: "Kjaerbye", age: "15", nationality: "DN", icon: "img/teams/astralis.png", region: "img/eu.gif", team: "Astralis"}
             ],
             players: [],
         }
@@ -83,6 +83,7 @@ export class FilteredList extends Component {
         );
     }
 }
+
 export class List extends Component {
     render(){
         return (
@@ -98,7 +99,9 @@ export class List extends Component {
                                         </div>
                                         <div className="large-3 column">{item.name}</div>
                                         <div className="large-2 column">{item.nationality}</div>
-                                        <div className="large-2 column">{item.region}</div>
+                                        <div className="large-2 column">
+                                            <img className="region" src={item.region} alt="Player Region"/>
+                                        </div>
                                         <div className="large-1 column">{item.age}</div>
                                     </div>
                                 </li>
